8d4254c18fa42c860d51c4d419dd85b3b00c4eee
andrea <andrea@trillonario.com>
2022-08-24 00:54:41 -0300

    Cambios

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import numpy as np
 import pandas as pd
 import plotly.express as px 
 import plotly.graph_objects as go 
+from mlxtend.frequent_patterns import apriori
+from mlxtend.frequent_patterns import association_rules
 
 st.set_page_config(
     page_title="Sistema de recomendación - Proyecto Final",
@@ -24,22 +26,64 @@ def get_data() -> pd.DataFrame:
 def get_data_items() -> pd.DataFrame:
     return pd.read_csv(dataset_url_items)
 
+def my_encode_units(x):
+    if x <= 0:
+        return 0
+    if x >= 1:
+        return 1    
+
 with header:
     st.title("Sistema de recomendación")
     st.sidebar.header('Sistema de recomendación')
-    st.markdown('El algoritmo a priori es uno de los más utilizados en este tema y permite encontrar de forma eficiente conjuntos de ítems frecuentes, los cuales sirven de base para generar reglas de asociación entre los ítems.')
+    #st.markdown('El algoritmo a priori es uno de los más utilizados en este tema y permite encontrar de forma eficiente conjuntos de ítems frecuentes, los cuales sirven de base para generar reglas de asociación entre los ítems.')
 
-with dataset:
+with features:
     st.header('Reglas de Asociación')
     st.markdown('Conceptos: ')
     st.markdown('* Soporte: El soporte del ítem es el número de transacciones que contienen X dividido entre el total de transacciones.')
     st.markdown('* Confianza: La confianza de una regla “Si X entonces Y” se define acorde a la ecuación')
     st.markdown('Confianza(X=>Y)=Soporte(unión(X,Y)) /soporte(X)')
 
-    dataset_url = "../data/raw/reglas_de_asociacion.csv"
-    dataset_url_items = "../data/raw/Dataset_itemsset.csv"
+with dataset:
+    dataset_url = "data/raw/reglas_de_asociacion.csv"
+    dataset_url_items = "data/raw/Dataset_itemsset.csv"
 
-    #st.subheader('')
+with model:
+    data=pd.read_csv('data/raw/Assignment-1_Data.csv',sep=';')
+    st.title('El modelo')
+    # Limpiamos los datos
+
+    data['Itemname'] = data['Itemname'].str.strip() # aca estamos remobiendo los espacios
+    data.dropna(axis=0, subset=['BillNo'], inplace=True) # elimino los na en las boletas
+    data.dropna(axis=0, subset=['CustomerID'], inplace=True) # elimino los na en los id de los clientes 
+    data['BillNo'] = data['BillNo']. astype('str') # convertimos el numero de boleta en string
+    data = data[~data['BillNo'].str.contains('C')] # removemos las posibles transacciones realizadas a credito
+    #data.head()
+
+    # Creamos el segundo dataset solo con los datos de Alemania
+    from itertools import groupby
+    dataset_germany = (data[data['Country']== "Germany"].groupby(['BillNo', 'Itemname'])['Quantity'].sum().unstack().reset_index().fillna(0).set_index('BillNo'))
+
+    #Vamos a definir una funcion que convertira todos los cero o menores que cero a cero y si son mayores a 1 en 1, 
+    # esto lo hacemos porque es lo que el algoritmo usa o espera como entrada
+
+    dataset_germany_set = dataset_germany.applymap(my_encode_units)
+    dataset_germany_set.drop('POSTAGE', inplace=True, axis=1)
+
+    df = dataset_germany_set.copy()
+
+    #Generamos la frecuencia de los items
+    df_itemsset = apriori(df, min_support=0.07, use_colnames=True)
+
+    # Creamos las reglas de asociacion
+    my_rules = association_rules(df_itemsset, metric='lift', min_threshold=1)
+
+    # Viasualizamos el top de las 100 reglas o normas de asociacion
+
+    #my_rules.head(100)
+
+    #my_rules[(my_rules['lift'] >= 3) & (my_rules['confidence'] >= 0.3)]   
+
 
 with interactive:
     st.title('La reglas')
