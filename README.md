8aa583e80e377e8de98ff4e8c4073681b6fe9581
andrea <andrea@trillonario.com>
2022-08-24 00:26:42 -0300

    Algunos cambios

--- README.md
+++ README.md
@@ -1,48 +1,24 @@
 # Machine Learning Final Project
 
-Integrantes del equipo:
+**Analisis de la Cesta de compra para el comercio minorista**
 
-Andrea Juarez
-Maria de los Angeles Gomez 
+Objetivo: las técnicas de ML implementadas buscan brindar informacion a los comerciantes en cuanto a las asociaciones observadas en las ventas de productos, asi como en las reglas que operan para la rotacion de los mismos.
+
+Alcance: El análisis parte de una limpieza y adecuacion de los datos, se aplica el aprendizaje con reglas de asociación donde lo  vemos aplicado principalmente en los sistemas de recomendación, como en el caso donde se nos muestra que las personas que compraron este producto, también compraron este otro o quienes vieron tal película también recomiendan estas otras, etc.
+
+El algoritmo a priori es uno de los más utilizados en este tema y permite encontrar de forma eficiente conjuntos de ítems frecuentes, los cuales
+sirven de base para generar reglas de asociación entre los ítems.
+
+Tipo de Aprendizaje: Supervisado
+
+Integrantes:
+
 Natia Lombardo
+Andrea Juarez
+Maria Gomez
 
+*Este Proyecto forma parte de los requerimientos para obtener el certificado de Machine Learning Engenire* en el marco del BOOTCAMP dictado por 4Geek Academy en Uruguay, asupiciado por la Universidad Tecnologica del Centro (UTEC) y el Banco Interamericano de Desarrollo.
 
+El repositorio puede encontrarlo en el siguiente link:
 
-Fuente : 
-
-
-Análisis y tecnicas implementadas : 
-
-a) Exploracion de Datos "EDA"
-b) Clustering
-c) Análisis de la Cesta de Compra (Algoritmo apriori)
-
-Modelo de asociacion
-nuestra api es un dashborad para mostrar info,  
-q se vende mas, tipo cerveza + pañales 
-prod distintis
-mettricas de prod mas vendidos
-como identificas
-
- y q productos se venden juntos los ubicamos juntos se venderian mas
- o tenemos un mini sistema de recomendacion 
-hisory telling (dashboard explicando) , strimling, vamos a mostrar recomendacion de productos (basado en algoritmo a priori)
-
- 
- este data set no sirve
-
-
-d) Tecnica de segmentacion RFM ?? capaz es mucho !
-e) API
-
-Resultados
-
-Conclusiones
-
-Documentación
-
-https://medium.com/@perfworld1/scraping-and-analyzing-online-marketplace-using-python-and-beautifulsoup-d38796b1606e
-
-
-https://www.databricks.com/notebooks/gallery/MarketBasket.html
-
+El sistema de recomendaciones puede encontrarlo en el siguiente link: 
--- src/app.py
+++ src/app.py
@@ -1,20 +1,71 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
+import plotly.express as px 
+import plotly.graph_objects as go 
 
-# Adding title and image
-st.title("Machine Learning Engineering - Final Proyect")
+st.set_page_config(
+    page_title="Sistema de recomendación - Proyecto Final",
+    page_icon="✅",
+    layout="wide",
+)
 
-st.write('Hola')
+header = st.container()
+dataset = st.container()
+features = st.container()
+model = st.container()
+interactive = st.container()
+footer = st.container()
 
-st.sidebar.header('')
+@st.cache
+def get_data() -> pd.DataFrame:
+    return pd.read_csv(dataset_url)
 
-https://github.com/ramonsaraiva/market-basket-analysis
+def get_data_items() -> pd.DataFrame:
+    return pd.read_csv(dataset_url_items)
 
-https://asagar60.medium.com/instacart-market-basket-analysis-part-3-deployment-ee813520284d
+with header:
+    st.title("Sistema de recomendación")
+    st.sidebar.header('Sistema de recomendación')
+    st.markdown('El algoritmo a priori es uno de los más utilizados en este tema y permite encontrar de forma eficiente conjuntos de ítems frecuentes, los cuales sirven de base para generar reglas de asociación entre los ítems.')
 
-https://towardsdatascience.com/introduction-to-simple-association-rules-mining-for-market-basket-analysis-ef8f2d613d87
+with dataset:
+    st.header('Reglas de Asociación')
+    st.markdown('Conceptos: ')
+    st.markdown('* Soporte: El soporte del ítem es el número de transacciones que contienen X dividido entre el total de transacciones.')
+    st.markdown('* Confianza: La confianza de una regla “Si X entonces Y” se define acorde a la ecuación')
+    st.markdown('Confianza(X=>Y)=Soporte(unión(X,Y)) /soporte(X)')
 
-https://github.com/topics/market-basket-analysis?o=desc&s=updated
+    dataset_url = "../data/raw/reglas_de_asociacion.csv"
+    dataset_url_items = "../data/raw/Dataset_itemsset.csv"
 
-Leonardo Toglia7:08 PM
-https://discuss.streamlit.io/t/feature-suggestion-image-carousel/1189
-https://github.com/DenizDogan92/Streamlit-Image-Carousel\
+    #st.subheader('')
+
+with interactive:
+    st.title('La reglas')
+    df = get_data()
+    #st.table(df)
+    fig = go.Figure(data = [go.Table(
+        header= dict(values=list(df[['antecedents','consequents','support','confidence','lift','leverage','conviction']].columns),
+        align = 'center'),
+        cells = dict(values=[df.antecedents,df.consequents,round(df.support,2),round(df.confidence,2),round(df.lift,2),round(df.leverage,2),round(df.conviction,2)]))
+    ])
+    fig.show()
+    st.write(fig)
+
+    st.title('Los items')
+    df = get_data_items()
+    #st.table(df)
+    fig = go.Figure(data = [go.Table(
+        header= dict(values=list(df[['support','itemsets']].columns),
+        align = 'center'),
+        cells = dict(values=[round(df.support,2),df.itemsets]))
+    ])
+    fig.show()
+    st.write(fig)
+
+
+
+
+with footer:
+    st.write('Fin')
